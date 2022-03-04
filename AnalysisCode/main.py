bc187e51fb28e1f136855b564391e38c2ac247a0
JSRuthe <42356585+JSRuthe@users.noreply.github.com>
2022-03-04 13:00:43 -0800

    Editing shutoff stamps for Bridger

--- AnalysisCode/main.py
+++ AnalysisCode/main.py
@@ -16,6 +16,8 @@ operatorDF, meterDF_All, sonicDF_All = loaddata()
     
 matchedDF_Bridger, matchedDF_GHGSat, matchedDF_CarbonMapper = performMatching(operatorDF, meterDF_All, sonicDF_All)
 
+
+
 csvPath = os.path.join(cwd, 'matchedDF_Bridger.csv')
 matchedDF_Bridger.to_csv(csvPath)
 
@@ -27,6 +29,34 @@ matchedDF_CarbonMapper.to_csv(csvPath)
 
 csvPath = os.path.join(cwd, 'meterDF_All.csv')
 meterDF_All.to_csv(csvPath)
+
+# COlumn names for export to teams:
+    
+cols = [
+    "Stanford_timestamp",
+    "cr_kgh_CH4_mean90", 
+    "cr_kgh_CH4_lower90", 
+    "cr_kgh_CH4_upper90", 
+    "FacilityEmissionRate", 
+    "FacilityEmissionRateUpper", 
+    "FacilityEmissionRateLower", 
+    "UnblindingStage", 
+    "PipeSize_inch", 
+    "MeterCode", 
+    "PlumeEstablished", 
+    "PlumeSteady", 
+    "cr_kgh_CH4_mean30", 
+    "cr_kgh_CH4_lower30", 
+    "cr_kgh_CH4_upper30", 
+    "cr_kgh_CH4_mean60", 
+    "cr_kgh_CH4_lower60", 
+    "cr_kgh_CH4_upper60", 
+    "Operator_Timestamp"]
+
+matchedDF_CarbonMapper_toTeam = matchedDF_CarbonMapper.reindex(cols)
+csvPath = os.path.join(cwd, 'matchedDF_CarbonMapper_unblindedToCM.csv')
+matchedDF_CarbonMapper_toTeam.to_csv(csvPath)
+
 
     # write matched results to csv
 #    cwd = os.getcwd()
